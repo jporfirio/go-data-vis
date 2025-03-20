fb4f7519d41c6387b4c2eb3cffa351008efefe99
Jeann Porfírio <jeannporfirio@gmail.com>
2025-03-20 18:05:24 -0300

    freecodecamp in go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jporfirio/go-dataviz
+
+go 1.23.5
+
+require github.com/a-h/templ v0.3.833 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/a-h/templ"
+)
+
+type DataFile struct {
+	Data [][2]any `json:"data"`
+}
+
+func main() {
+	file, err := os.ReadFile("data.json")
+	if err != nil {
+		log.Fatalf("Error reading file: %v\n", err)
+	}
+
+	var data DataFile
+	if err := json.Unmarshal(file, &data); err != nil {
+		log.Fatalf("Error decoding JSON: %v\n", err)
+	}
+
+	component := Chart(data.Data)
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Printf("Listening on port :%d\n", 8080)
+
+	http.ListenAndServe(":8080", nil)
+}
